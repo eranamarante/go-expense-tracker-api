ac1be8ab0e69a8b2a31f01be9566264af7dca3c6
Eran Amarante <eran.amarante@yahoo.com.ph>
2021-07-02 16:55:32 +0800

    add get all expense

---
+++ server/middleware/expenseMiddleware.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/eranamarante/go-react-expense-tracker/server/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var expensesCollection *mongo.Collection = database.OpenCollection(database.Client, "expenses")
+
+func GetAllExpenses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	payload := getAllExpenses()
+	json.NewEncoder(w).Encode(payload)
+}
+
+func getAllExpenses() []primitive.M  {
+	findOptions := options.Find()
+	findOptions.SetSort(bson.D{primitive.E{Key: "_id", Value: -1}})
+
+	cur, err := expensesCollection.Find(context.Background(), bson.D{}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []primitive.M
+	for cur.Next(context.Background()) {
+		var result bson.M
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		results =  append(results, result)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cur.Close(context.Background())
+	return results
+}
