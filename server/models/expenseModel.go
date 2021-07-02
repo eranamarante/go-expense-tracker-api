aa1e6330e04867d9d9e421a79740904013aeb142
Eran Amarante <eran.amarante@yahoo.com.ph>
2021-07-02 15:47:18 +0800

    add expense model

---
+++ server/models/expenseModel.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Expense struct {
+	Id    			primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
+	Title 			string 							`json:"title" bson:"title"`
+	Amount			float64							`json:"amount" bson:"amount"`
+	DueDate			time.Time						`json:"due_date" bson:"due_date"`
+	Status			bool								`json:"status" bson:"status"`
+	CreatedAt 	time.Time						`json:"created_at" bson:"created_at"`
+	UpdatedAt 	time.Time   				`json:"updated_at" bson:"updated_at"`
+}
