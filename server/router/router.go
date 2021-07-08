0d4e468e181798f587584118fc409265787f8379
Eran Amarante <eran.amarante31@gmail.com>
2021-07-08 13:22:09 +0800

    add get and delete func

--- server/router/router.go
+++ server/router/router.go
@@ -11,13 +11,13 @@ func Router() *mux.Router {
 	// Auth Endpoints
 	// r.HandleFunc("api/auth/login", authMiddleware.Login).Methods("POST", "OPTIONS")
 	// r.HandleFunc("api/auth/login", authMiddleware.Signup).Methods("POST", "OPTIONS")
-	
+
 	// Expense Endpoints
-	r.HandleFunc("/api/expenses", middleware.GetAllExpenses).Methods("GET", "OPTIONS")
-	// r.HandleFunc("api/expenses/{id}", middleware.GetExpense).Methods("GET", "OPTIONS")
-	r.HandleFunc("/api/expenses/new", middleware.AddExpense).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/expenses", middleware.GetAllExpenses).Methods("GET")
+	r.HandleFunc("/api/expenses/{id}", middleware.GetExpense).Methods("GET")
+	r.HandleFunc("/api/expenses/new", middleware.AddExpense).Methods("POST")
 	// r.HandleFunc("api/expenses/{id}/edit", middleware.UpdateExpense).Methods("PUT", "OPTIONS")
-	// r.HandleFunc("api/expenses/{id}/delete", middleware.DeleteExpense).Methods("DELETE", "OPTIONS")
+	r.HandleFunc("/api/expenses/{id}/delete", middleware.DeleteExpense).Methods("DELETE")
 
 	return r
 }
