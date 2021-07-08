76d01733a148b56589f62637a83b172a1933031e
Eran Amarante <eran.amarante31@gmail.com>
2021-07-08 13:20:06 +0800

    update database config

--- server/database/database.go
+++ server/database/database.go
@@ -4,28 +4,39 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/eranamarante/go-react-expense-tracker/server/helper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnectDB() *mongo.Collection {
+func DBInstance() *mongo.Client {
 	config := helper.GetConfiguration()
 
-	// Set client options
-	clientOptions := options.Client().ApplyURI(config.ConnectionString)
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
+	client, err := mongo.NewClient(options.Client().ApplyURI(config.ConnectionString))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Connected to MongoDB!")
 
-	collection := client.Database("go-expense-tracker").Collection("expenses")
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	config := helper.GetConfiguration()
+
+	var collection *mongo.Collection = client.Database(config.DatabaseName).Collection(collectionName)
 
 	return collection
 }
