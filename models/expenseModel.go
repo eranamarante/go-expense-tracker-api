c2ed6d3e9d1d27eefd410f0c486f33bbf8dc9d99
Eran Amarante <eran.amarante31@gmail.com>
2021-07-12 18:08:45 +0800

    update models

--- models/expenseModel.go
+++ models/expenseModel.go
@@ -8,11 +8,11 @@ import (
 
 type Expense struct {
 	Id          primitive.ObjectID `bson:"_id"`
-	Description string             `json:"description"`
-	Amount      float64            `json:"amount"`
-	DueDate     time.Time          `json:"due_date"`
-	IsPaid      bool               `json:"is_paid"`
-	EnteredBy   *User              `json:"entered_by"`
-	CreatedAt   time.Time          `json:"created_at"`
-	UpdatedAt   time.Time          `json:"updated_at"`
+	Description string             `json:"description" bson:"description"`
+	Amount      float64            `json:"amount" bson:"amount"`
+	DueDate     time.Time          `json:"due_date" bson:"due_date"`
+	IsPaid      bool               `json:"is_paid" bson:"is_paid"`
+	EnteredBy   *User              `json:"entered_by" bson:"entered_by"`
+	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
 }
