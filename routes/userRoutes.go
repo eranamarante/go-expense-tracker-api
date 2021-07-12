03eec1d2d17d99b28f1ae7394416e0214be73fe1
Eran Amarante <eran.amarante31@gmail.com>
2021-07-12 16:06:53 +0800

    add user routes

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -1,13 +1,11 @@
 package routes
 
 import (
-	controller "github.com/eranamarante/go-expense-tracker-api/controllers"
-
+	"github.com/eranamarante/go-expense-tracker-api/controllers"
 	"github.com/gin-gonic/gin"
 )
 
-//UserRoutes function
-func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("/users/signup", controller.SignUp())
-	incomingRoutes.POST("/users/login", controller.Login())
+func UserRoutes(route *gin.Engine) {
+	route.POST("/users/signup", controllers.SignUp())
+	route.POST("/users/login", controllers.Login())
 }
